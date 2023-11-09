3b16397ffe13b1da51b9f6b598eee6728e7c0ca8
Muchammad Indra <muchammadindra97@gmail.com>
2023-11-09 18:22:50 +0700

    feat: delete book by id

--- src/handler.js
+++ src/handler.js
@@ -161,9 +161,33 @@ const editBookByIdHandler = (request, h) => {
   return response
 }
 
+const deleteBookByIdHandler = (request, h) => {
+  const { id } = request.params
+
+  const bookIndex = books.findIndex(book => book.id === id)
+
+  if (bookIndex !== -1) {
+    books.splice(bookIndex, 1)
+
+    return {
+      status: 'success',
+      message: 'Buku berhasil dihapus'
+    }
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan'
+  })
+
+  response.code(404)
+  return response
+}
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
-  editBookByIdHandler
+  editBookByIdHandler,
+  deleteBookByIdHandler
 }
