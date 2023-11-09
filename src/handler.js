71a211e087a4e10e43df0ba8d7770c89e3ce68fc
Muchammad Indra <muchammadindra97@gmail.com>
2023-11-09 18:16:20 +0700

    feat: edit book by id

--- src/handler.js
+++ src/handler.js
@@ -95,8 +95,75 @@ const getBookByIdHandler = (request, h) => {
   return response
 }
 
+const editBookByIdHandler = (request, h) => {
+  const { id } = request.params
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading
+  } = request.payload
+
+  if (typeof name === 'undefined' || name === null) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  const bookIndex = books.findIndex(b => b.id === id)
+
+  if (bookIndex !== -1) {
+    const updatedAt = new Date().toISOString()
+
+    books[bookIndex] = {
+      ...books[bookIndex],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt
+    }
+
+    return {
+      status: 'success',
+      message: 'Buku berhasil diperbarui'
+    }
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan'
+  })
+
+  response.code(404)
+  return response
+}
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
-  getBookByIdHandler
+  getBookByIdHandler,
+  editBookByIdHandler
 }
